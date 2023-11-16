7e39aa0ba3a702f7082b37dbc42563c5b0110457
Kate Ridgle <kroo.2394@gmail.com>
2023-11-15 21:04:20 -0800

    working on p router

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,3 +1,41 @@
 // add middlewares here related to projects
 
+const projects = require('./projects-model')
 
+async function checkProjectId(req,res,next) {
+    try{
+        const project = await projects.get(req.params.id);
+        if(project){
+            req.project = project;
+            next();
+
+        }
+        else {
+            next({status:404, message:'Project not found'})
+        }
+    }
+    catch(err){
+        next(err)
+    }
+}
+
+function checkProjectBody(req,res,next){
+    const{name, descpription}=req.body
+    if(name==undefined || typeof name !== 'string' || !name.trim().length){
+        next({status: 400, message: 'requires name'})
+    } 
+    else if (descpription==undefined || typeof descpription !== 'string'|| !descpription.trim().length) {
+        next({status: 400, message: 'requires description'})
+    }
+    else {
+        req.name = name.trim()
+        req.descpription = descpription.trim()
+        next()
+    }
+
+}
+
+module.exports={
+    checkProjectId,
+    checkProjectBody
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,3 +1,43 @@
 // Write your "projects" router here!
 
 //module 2 objective 2?// same for action routers?//
+
+const express = require('express');
+const Project = require('./projects-model');
+
+const {checkProjectId, checkProjectBody} = require('./projects-middleware');
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router;
